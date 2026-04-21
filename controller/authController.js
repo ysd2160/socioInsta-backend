51930f0a5025620270be9e1ab189129f47f07f07
ysd2160 <ysd2445@gmail.com>
2026-04-21 16:50:15 +0530

    11 commit

--- controller/authController.js
+++ controller/authController.js
@@ -27,62 +27,68 @@ export const Register = async (req, res) => {
 }
 
 export const Login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+        const user = await User.findOne({ email });
 
-    if (!user) {
-      return res.json({
-        status: 400,
-        message: "user not exists",
-      });
+        if (!user) {
+            return res.json({
+                status: 400,
+                message: "user not exists",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        console.log({
+            entered: JSON.stringify(password),
+            hash: user.password,
+            hashLength: user.password.length,
+            isMatch,
+            email: user.email
+        });
+        if (!isMatch) {
+            return res.json({
+                status: 400,
+                message: "invalid Credentials",
+            });
+        }
+
+        const token = jwt.sign(
+            { user: user },
+            "secret",
+            { expiresIn: "1d" }
+        );
+
+        return res
+            .cookie("token", token, {
+                httpOnly: true,
+                secure: false, // local ke liye
+                sameSite: "Lax",
+            })
+            .json({
+                status: 200,
+                message: "Login successfully",
+                user,
+            });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server error" });
     }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.json({
-        status: 400,
-        message: "invalid Credentials",
-      });
-    }
-
-    const token = jwt.sign(
-      { user: user },
-      "secret",
-      { expiresIn: "1d" }
-    );
-
-    return res
-      .cookie("token", token, {
-        httpOnly: true,
-        secure: false, // local ke liye
-        sameSite: "Lax",
-      })
-      .json({
-        status: 200,
-        message: "Login successfully",
-        user,
-      });
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Server error" });
-  }
 };
 export const Logout = async (req, res) => {
     try {
         return res
-      .clearCookie("token", {
-        httpOnly: true,
-        secure: false, // same as login
-        sameSite: "Lax"
-      })
-      .status(200)
-      .json({
-        message: "logout successfully"
-      });
+            .clearCookie("token", {
+                httpOnly: true,
+                secure: false, // same as login
+                sameSite: "Lax"
+            })
+            .status(200)
+            .json({
+                message: "logout successfully"
+            });
     } catch (error) {
 
     }
